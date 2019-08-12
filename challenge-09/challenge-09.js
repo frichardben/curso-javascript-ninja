c41e4bfc7de10b65a3b93e66d3c7addeb38cd96f
Richard Ben <frichardben@yahoo.com.br>
2019-08-12 06:32:44 -0300

    Update challenge-09.js

--- challenge-09/challenge-09.js
+++ challenge-09/challenge-09.js
@@ -60,13 +60,19 @@ o retorno de `calculator`.
 por parâmetro, INVOCADA, e passando a ela por parâmetro os dois valores
 que foram passadas para a primeira função `calculator`.
 */
-// ?
+function calculator(num1,num2){
+  return function(callback){
+    return callback(num1,num2);
+  };
+}
+
 
 /*
 Declare uma variável chamada `sum`, e atribua a ela a função `calculator`,
 passando dois números por parâmetro.
 */
-// ?
+
+var sum = calculator(4,2);
 
 /*
 Sabemos que `sum` agora tem uma função atribuída a ela, que é o retorno de
@@ -78,14 +84,19 @@ uma função anônima que irá retornar a soma dos dois números que essa funç�
 anônima tem como seus argumentos.
 */
 console.log( 'O resultado da soma é:' );
-// ?
+console.log(sum(function(number1,number2){
+   return number1 + number2;
+}) );
 
 /*
 Agora declare outra variáveis chamadas `subtraction`, `multiplication`,
 `division` e `mod`, e atribua à elas `calculator`, passando números
 diferentes para cada chamada.
 */
-// ?
+var subtraction = calculator(5,4);
+var multiplication = calculator(6,10);
+var division = calculator(15,3);
+var mod = calculator(16,8);
 
 /*
 Mostre as variáveis acima no `console` (uma chamada de console por variável),
@@ -95,13 +106,25 @@ As suas respostas devem estar abaixo dos `console.log` referentes à cada
 chamada.
 */
 console.log( 'O resultado da subtração é:' );
-// ?
+console.log(subtraction(function(number1,number2){
+  return number1 - number2;
+  
+}) );
 
 console.log( 'O resultado da multiplicação é:' );
-// ?
+console.log(multiplication(function(number1,number2){
+  return number1 * number2;
+
+}) );
 
 console.log( 'O resultado da divisão é:' );
-// ?
+console.log(division(function(number1,number2){
+  return number1 / number2;
+  
+}) );
 
 console.log( 'O resto da divisão é:' );
-// ?
+console.log(mod(function(number1,number2){
+  return number1 % number2;
+
+}) );
